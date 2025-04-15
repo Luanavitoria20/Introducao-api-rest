5f9dd10489a4a1828ccbf88257f15a5d0a2cd8d0
Luana vitoria <luanavitoriasantosaraujo8@gmail.com>
2025-04-15 13:36:09 -0300

    atualizar app.js

--- app.js
+++ app.js
@@ -5,9 +5,15 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {  
-    res.send('Hello World!')
+    res.send('Olá World!')
 
 })
+
+app.get('/', (req, res) => {  
+  res.send('Olá Usuário!')
+})
+
+app.post("/criarUsuário",()=>{})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
