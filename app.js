bdd1be7d0fc389726a0a0cf7e5a52663d6c64bfa
Luana vitoria <luanavitoriasantosaraujo8@gmail.com>
2025-04-15 16:37:34 -0300

    feat: get all users eadpoint

--- app.js
+++ app.js
@@ -2,19 +2,47 @@ import express from 'express'
 
 const app = express()
 
+//permitir ler JSON no corpo da requisição
+app.use(express.json())
+
 const port = 3000
+//Banco de dados Fake(em memória) 
+const usuarios = [
+  {id:1, nome: 'João', email: "joao@gmail.com"},
+  {id:2, nome: 'Ana', email: "ana@gmail.com"}
+]
 
-app.get('/', (req, res) => {  
+app.get("/", (req, res) => {  
+  res.send("Bem vindo a minha API!")
+})
+
+/*app.get('/', (req, res) => {  
     res.send('Olá World!')
-
 })
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+}
+)*/
 
-app.get("/Usuário", (req, res) => {  
-  res.send('Olá Usuário!')
-})
-
-app.post("/criarUsuário",()=>{})
+/*
+Crie uma rota (endopoint) do tipo GETcom URI:/usuarios que 
+envie uma resposta(mensagem) com todos os 
+usuarios cadastrasda no "banco de dados fake"
+*/
+app.get('/usuarios', (req, res) => {  
+  res.send(usuarios)
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+}
+)
+
+/*app.post("/criarUsuario", (req, res)=> {
+  const {nome, sobrenome} = req.body
+  res.send(`Nome:${nome} | Sobrenome:${sobrenome}`)
 })
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})  
+*/
