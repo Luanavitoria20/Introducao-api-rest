2157045e36dc0a32731017034663d7ae8885d5bf
Luana vitoria <luanavitoriasantosaraujo8@gmail.com>
2025-04-22 13:24:32 -0300

    novo

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import express from 'express'
 const app = express()
 
 //permitir ler JSON no corpo da requisição
+
 app.use(express.json())
 
 const port = 3000
@@ -12,11 +13,60 @@ const usuarios = [
   {id:2, nome: 'Ana', email: "ana@gmail.com"}
 ]
 
+//Rota inicio
 app.get("/", (req, res) => {  
   res.send("Bem vindo a minha API!")
 })
 
-/*app.get('/', (req, res) => {  
+//Rota cria Usuario
+app.post("/criarUsuario", (req, res)=> {
+  // adicionar o usuario no banco de dados fake (lista)
+  const {nome, email} = req.body
+
+  const novoUsuario = {
+    id : usuarios[usuarios.length-1].id + 1,
+    nome: nome,
+    email: email
+  };
+
+  usuarios.push(novoUsuario)
+  
+  res.send(usuarios)
+})
+
+app.put("/usuario/:id", (req, res)=>{
+  const { id } = req.params
+  const {novoNome, novoEmail} = req.body
+
+  const indice = usuarios.findIndex((usuario)=>{
+    return usuario.id == id
+  })
+
+  usuarios[indice].nome = novoNome
+  usuarios[indice].email = novoEmail
+
+  res.send(usuarios)
+})
+
+/*
+Faça uma rota para deletar um usuario de acordo com o id 
+recebido por parametro
+Método: DELETE
+Enpoint: /usuario/id:
+resposta: a lista de usuarios atualizada
+dica: use o splice() para remover o usuario da lista
+*/
+app.delete('/usuario/:id', (req, res) => {
+  const id = parseInt(req.params.id); 
+  const index = usuarios.findIndex(usuario => usuario.id === id);
+  usuarios.splice(index,1)
+  res.send(usuarios)
+});
+
+app.listen(port,()=>{
+  console.log(`App escutando na porta $:(port)`)
+})
+/* app.get('/', (req, res) => {  
     res.send('Olá World!')
 })
 app.listen(port, () => {
@@ -38,11 +88,3 @@ app.listen(port, () => {
 }
 )
 
-/*app.post("/criarUsuario", (req, res)=> {
-  const {nome, sobrenome} = req.body
-  res.send(`Nome:${nome} | Sobrenome:${sobrenome}`)
-})
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})  
-*/
